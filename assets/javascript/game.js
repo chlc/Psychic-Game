e88acdeac243a536a9fd94bb4df8ab3fdda5075b
Chelsea <TheGoldess@The-Goldess-2.local>
2017-09-12 23:55:11 -0500

    edited game.js to include variables, letter array, and have computer guess a letter

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+//List of Avaialable Letters
+  var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
+
+//Initializing all of the variables
+var wins = 0;
+var losses = 0;
+var guesses = 9;
+var guessesLeft = 9;
+var guessedLetters = [];
+var letterToGuess = null;
+
+//Have computer choose random letter
+var cGuess = letters[Math.floor(Math.random() * letters.length)];
+	console.log(cGuess);
+
+//
