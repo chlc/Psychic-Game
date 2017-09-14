709032e34aafbd14ce9c0c229cbb318c436bb454
Chelsea <TheGoldess@The-Goldess-2.local>
2017-09-14 15:30:43 -0500

    rearranged variable order

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,3 @@
-//List of Avaialable Letters
-  var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
-
 //Initializing all of the variables
 var wins = 0;
 var losses = 0;
@@ -8,6 +5,9 @@ var guesses = 9;
 var guessesLeft = 9;
 var guessedLetters = [];
 var letterToGuess = null;
+
+//List of Avaialable Letters
+  var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
 
 //Have computer choose random letter
 var compGuess = letters[Math.floor(Math.random() * letters.length)];
