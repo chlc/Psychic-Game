521eadb41606317f5110e08dd1af4931810ec63e
Chelsea <TheGoldess@The-Goldess-2.local>
2017-09-14 15:06:26 -0500

    put back my code still not working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,7 @@
+//List of Avaialable Letters
+  var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
+
+//Initializing all of the variables
 var wins = 0;
 var losses = 0;
 var guesses = 9;
@@ -5,65 +9,63 @@ var guessesLeft = 9;
 var guessedLetters = [];
 var letterToGuess = null;
 
-var computerLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+//Have computer choose random letter
+var compGuess = letters[Math.floor(Math.random() * letters.length)];
+	console.log(compGuess);
 
-var computerGuess = computerLetters[Math.floor(Math.random()*computerLetters.length)];
-	console.log(computerGuess);
-
-var updateGuessesLeft = function() {
+//Updated how many guesses are left
+var changeGuessesLeft = function() {
 	document.querySelector('#guessesLeft').innerHTML = "Guesses left: " + guessesLeft;
 };
 
-var updateLetterToGuess = function() {
-  this.letterToGuess = this.computerLetters[Math.floor(Math.random() * this.computerLetters.length)];
+//Computer keeps guessing
+var changeLetterGuess = function (){
+	this.letterToGuess = this.letters[Math.floor(Math.random() * this.letters.length)];
 };
 
-var updateGuessedLetters = function() {
-	// Display user's guesses.
-	document.querySelector('#guessedLetters').innerHTML = "Guessed Letters: " + guessedLetters;
-
+//Showing what the user input is
+var changeGuessedLetters = function(){
+document.querySelector('#guessedLetters').innerHTML = "Guessed Letters: " + guessedLetters;
 };
 
-// Reset function
+
+//Resetting the game
 var reset = function() {
 	totalGuesses = 9;
 	guessesLeft = 9;
-	guessedLetters = [];
+	guessedLetters= [];
 
-	updateGuessesLeft();
-	updateGuessedLetters();
-	updateLetterToGuess();
+	changeGuessedLetters();
+	changeGuessesLeft();
+	changeLetterGuess();
 }
 
-updateLetterToGuess();
+changeLetterGuess();
+
+//onKeyUp event to identify user input
+document.onKeyUp = function() {
+	guessesLeft--;
+
+var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+console.log(userGuess);
+guessedLetters.push(userGuess);
+changeGuessedLetters();
+changeGuessesLeft();
+
+if (guessesLeft > 0){
+	if (userGuess===compGuess){
+		wins++, reset();
+		alert("Your power is terrifying.");
+		document.querySelector('#wins').innerHTML = "Wins: " + wins;
+
+	}
+} else if (guessesLeft == 0){
+	losses++, reset();
+	document.querySelector('#losses').innerHTML = "Losses: " + losses;
+	alert("This is embarrassing...the correct letter was " + letterToGuess + ".");
 
 
-document.onkeyup = function() {
-		guessesLeft--;
-	// Takes user's guess and ensures it is converted to lower case
-	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-		// Testing -- remove
-		console.log(userGuess);
+}
 
-	guessedLetters.push(userGuess);
-	updateGuessedLetters();
-	updateGuessesLeft();
-
-		if (guessesLeft > 0){
-			// User guesses correctly
-            if (userGuess===computerGuess){
-                wins++, reset();
-                alert("You truly are a psychic."); 
-                document.querySelector('#wins').innerHTML = "Wins: " + wins;
-                displayWinPsychic();
-                       
-        } 
-
-        } else if (guessesLeft == 0){
-            // Lose and update the loss 
-            losses++, reset();
-            document.querySelector('#losses').innerHTML = "Losses: " + losses;
-            alert("You are not a psychic. The letter was " + letterToGuess + ".");
-            displayLosePsychic();
-        }    
 };
