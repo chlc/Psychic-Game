640f7057477f570e062d4d341496464ac70604fb
Chelsea <TheGoldess@The-Goldess-2.local>
2017-09-14 14:43:12 -0500

    win lose stuff

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,14 +14,41 @@ var compGuess = letters[Math.floor(Math.random() * letters.length)];
 	console.log(compGuess);
 
 //onKeyUp event to identify user input
-document.onKeyUp = function(event) {
+document.onKeyUp = function() {
+	guessesLeft--;
 
 var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+console.log(userGuess);
+guessedLetters.push(userGuess);
+changeGuessedLetters();
+changeGuessesLeft();
+
+if (guessesLeft > 0){
+	if (userGuess === compGuess){
+		wins++, reset();
+		alert("Your power is terrifying.");
+		document.querySelector('#wins').innerHTML = "Wins: " + wins;
+
+	}
+} else if (guessesLeft == 0){
+	losses++, reset();
+	document.querySelector('#losses').innerHTML = "Losses: " + losses;
+	alert("This is embarrassing...the correct letter was " + letterToGuess + ".");
+
+
+}
+
 }
 //Showing what the user input is
 document.querySelector('#guessedLetters').innerHTML = "Guessed Letters: " + guessedLetters;
 
 //Updated how many guesses are left
 var changeGuessesLeft = function() {
-	document.querySelector('#guessesLeft').innerHTML = "Guessese left: " + guessesLeft;
+	document.querySelector('#guessesLeft').innerHTML = "Guesses left: " + guessesLeft;
+}
+
+//Computer keeps guessing
+var changeLetterGuess = function (){
+	this.letterToGuess = this.letters[Math.floor(Math.random() * letters.length)]
 }
